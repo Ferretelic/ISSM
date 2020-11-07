068ee35e2ef1c28b28d70a947ca67623660dc128
DOALA0155 <kyleshouki@gmail.com>
2020-11-07 16:41:00 +0900

    created submission file

---
+++ Keras/Unbalanced/SimpleConvDataAugmentationExpand/model.py
@@ -0,0 +1,40 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, Dropout, MaxPooling2D, GlobalAveragePooling2D, Dense, BatchNormalization
+
+def cnn_model():
+  model = Sequential()
+
+  model.add(Conv2D(64, (3, 3), padding="same", activation="relu", input_shape=(64, 64, 3)))
+  model.add(Conv2D(64, (3, 3), padding="same", activation="relu"))
+  model.add(BatchNormalization())
+  model.add(Conv2D(64, (3, 3), padding="same", activation="relu"))
+  model.add(MaxPooling2D())
+  model.add(Dropout(0.25))
+
+  model.add(Conv2D(128, (3, 3), padding="same", activation="relu"))
+  model.add(Conv2D(128, (3, 3), padding="same", activation="relu"))
+  model.add(BatchNormalization())
+  model.add(Conv2D(128, (3, 3), padding="same", activation="relu"))
+  model.add(MaxPooling2D())
+  model.add(Dropout(0.25))
+
+  model.add(Conv2D(256, (3, 3), padding="same", activation="relu"))
+  model.add(Conv2D(256, (3, 3), padding="same", activation="relu"))
+  model.add(BatchNormalization())
+  model.add(Conv2D(256, (3, 3), padding="same", activation="relu"))
+  model.add(Conv2D(256, (3, 3), padding="same", activation="relu"))
+  model.add(Conv2D(256, (3, 3), padding="same", activation="relu"))
+  model.add(BatchNormalization())
+  model.add(Conv2D(256, (3, 3), padding="same", activation="relu"))
+  model.add(Conv2D(256, (3, 3), padding="same", activation="relu"))
+  model.add(GlobalAveragePooling2D())
+
+  model.add(Dense(1024, activation="relu"))
+  model.add(Dropout(0.5))
+  model.add(Dense(1024, activation="relu"))
+  model.add(Dropout(0.5))
+  model.add(Dense(10, activation="softmax"))
+
+  model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+  return model
