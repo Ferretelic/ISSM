c7b86412907915143ab3731dccd938539a26ef4e
Ferretelic <kyleshouki@gmail.com>
2020-11-26 17:36:27 +0900

    created ensemble submission

--- Keras/Unbalanced/Advanced/Ensemble/compare_submissions.py
+++ Keras/Unbalanced/Advanced/Ensemble/compare_submissions.py
@@ -7,11 +7,12 @@ from keras.models import load_model
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+import json
 
 physical_devices = tf.config.experimental.list_physical_devices("GPU")
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-def ensemble_models(models):
+def ensemble_models(models, id):
   _, test_ids = load_test_dataset(size=64)
   predictions = np.zeros((test_ids.shape[0], 10))
   submission = np.empty((test_ids.shape[0]))
@@ -32,13 +33,10 @@ def ensemble_models(models):
 
   submission_csv = pd.read_csv("/home/shouki/Desktop/Programming/Python/AI/Datasets/ImageData/ISSM/sampleSolution.csv")
   submission_csv["LABEL"] = np.array(submission, dtype=np.int32)
-  submission_csv.to_csv("./submission/submission.csv", index=False)
+  submission_csv.to_csv("./submission/submission_{}.csv".format(id), index=False)
 
-models = [
-  ["Model5000Size64", "100"],
-  ["Model10000Size64", "150"],
-  ["Model10000Size72", "75"],
-  ["Model20000Size64", "175"]
-]
+ensemble_id = "1"
+with open("./submission/submissions.json", "r") as f:
+  models = json.load(f)[ensemble_id]
 
-ensemble_models(models)
+ensemble_models(models, ensemble_id)
