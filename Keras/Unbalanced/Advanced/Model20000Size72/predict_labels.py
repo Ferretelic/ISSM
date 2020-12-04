9f726fe4431a46cb6d7812840a9aa96a13ceec6d
Ferretelic <kyleshouki@gmail.com>
2020-12-04 22:24:01 +0900

    created submissions

---
+++ Keras/Unbalanced/Advanced/Model20000Size72/predict_labels.py
@@ -0,0 +1,27 @@
+from issm import load_test_dataset
+from keras.utils import to_categorical
+from keras.models import load_model
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+physical_devices = tf.config.experimental.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+def create_submission_file(epoch):
+  x_test, test_ids = load_test_dataset(size=72)
+
+  model = load_model("./models/model_{}.h5".format(epoch))
+  predictions = np.argmax(model.predict(x_test), axis=1)
+
+  submission = np.empty((predictions.shape[0]))
+
+  for label, image_id in zip(predictions, test_ids):
+    submission[image_id - 1] = label + 1
+
+  submission_csv = pd.read_csv("/home/shouki/Desktop/Programming/Python/AI/Datasets/ImageData/ISSM/sampleSolution.csv")
+  submission_csv["LABEL"] = np.array(submission, dtype=np.int32)
+  submission_csv.to_csv("./submission/submission_{}.csv".format(epoch), index=False)
+
+for epoch in ["25", "50", "75", "100", "125"]:
+  create_submission_file(epoch)p
---
+++ Keras/Unbalanced/Advanced/Model20000Size72/train_cnn.py
@@ -0,0 +1,40 @@
+from unbalance import load_regulized_train_dataset
+from history import show_history
+from keras.utils import to_categorical
+from model import cnn_model
+from keras.callbacks import ModelCheckpoint, TensorBoard, LearningRateScheduler
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import pickle
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+physical_devices = tf.config.experimental.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+batch_size = 72
+epochs = 200
+samples = 20000
+size = 64
+input_size = (size, size, 3)
+
+x_train, y_train = load_regulized_train_dataset(samples, size)
+y_train = to_categorical(y_train)
+x_train, x_validation, y_train, y_validation = train_test_split(x_train, y_train, test_size=0.2, stratify=y_train)
+
+model = cnn_model(input_size)
+
+callbacks = [
+  ModelCheckpoint(filepath="./models/model_{epoch:02d}.h5"),
+  TensorBoard(log_dir="./logs")
+]
+
+model_history = model.fit(x_train, y_train, epochs=epochs, callbacks=callbacks, validation_data=(x_validation, y_validation))
+
+model.save("./models/model_final.h5")
+history = model_history.history
+
+with open("./history/model_history.pkl", "wb") as f:
+  pickle.dump(history, f)
+
+show_history(history)
